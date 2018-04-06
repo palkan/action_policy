ce678cb47dac100c05e3a5ec8f0aad222d41f4ec
Vladimir Dementyev <dementiev.vm@gmail.com>
2018-04-06 12:53:12 +0300

    Add auto_inject_into_controller config

--- lib/action_policy/railtie.rb
+++ lib/action_policy/railtie.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
-module ActionPolicy
+module ActionPolicy # :nodoc:
+  class << self
+    # Define whether we need to extend ApplicationController::Base
+    # with the default authorization logic.
+    attr_accessor :auto_inject_into_controller
+  end
+
+  self.auto_inject_into_controller = true
+
   class Railtie < ::Rails::Railtie # :nodoc:
-    initializer "action_policy.controller" do |_app|
+    config.after_initialize do |_app|
       ActiveSupport.on_load(:action_controller) do
+        next unless ActionPolicy.auto_inject_into_controller
+
         require "action_policy/rails/controller"
 
         ActionController::Base.include ActionPolicy::Controller
