2590be5df9ec014dbb60c25fe3b3db52417b0f8d
Vladimir Dementyev <dementiev.vm@gmail.com>
2018-08-14 15:41:45 -0400

    Extract did_you_mean suggestions into a module; add suggestions to scope exceptions

---
+++ lib/action_policy/utils/suggest_message.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module ActionPolicy
+  # Adds `suggest` method which uses did_you_mean
+  # to generate a suggestion message
+  module SuggestMessage
+    if defined?(::DidYouMean::SpellChecker)
+      def suggest(needle, heystack)
+        suggestion = ::DidYouMean::SpellChecker.new(
+          dictionary: heystack
+        ).correct(needle).first
+
+        suggestion ? "\nDid you mean? #{suggestion}" : ""
+      end
+    else
+      def suggest(*)
+        ""
+      end
+    end
+  end
+end
