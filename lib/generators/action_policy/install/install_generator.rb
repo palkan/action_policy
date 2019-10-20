4af4c58f6fc454a4eed8a084e84b0599e993cb44
Nicolas Brousse <n.brousse@pantographe.studio>
2019-10-20 16:08:20 +0200

    Create action_policy:install rails generator

---
+++ lib/generators/action_policy/install/install_generator.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module ActionPolicy
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("templates", __dir__)
+
+      def copy_application_policy
+        template "application_policy.rb", "app/policies/application_policy.rb"
+      end
+    end
+  end
+end
