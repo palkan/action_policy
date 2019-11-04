716563c1e692841785d89e00451cafc12fb14625
Nicolas Brousse <n.brousse@pantographe.studio>
2019-11-04 22:56:06 +0100

    Invoke action_policy:install on policy generator

--- lib/generators/action_policy/policy/policy_generator.rb
+++ lib/generators/action_policy/policy/policy_generator.rb
@@ -7,6 +7,8 @@ module ActionPolicy
     class PolicyGenerator < ::Rails::Generators::NamedBase
       source_root File.expand_path("templates", __dir__)
 
+      invoke "action_policy:install"
+
       def create_policy
         template "policy.rb", File.join("app/policies", class_path, "#{file_name}_policy.rb")
       end
