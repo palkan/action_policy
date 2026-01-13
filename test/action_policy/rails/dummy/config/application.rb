da9a158425a5355430c8d2832f22e682f451ef74
Vladimir Dementyev <dementiev.vm@gmail.com>
2026-01-13 15:19:48 +0300

    * test: use default Rails configuration for the current version

--- test/action_policy/rails/dummy/config/application.rb
+++ test/action_policy/rails/dummy/config/application.rb
@@ -11,6 +11,9 @@ Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
+    config.load_defaults [Rails::VERSION::MAJOR, Rails::VERSION::MINOR].map(&:to_s).join(".").to_f
+    config.action_controller.allow_forgery_protection = false
+
     # Hack for Rails 7 alpha
     config.active_record = ActiveSupport::OrderedOptions.new
     config.logger = Logger.new("/dev/null") # rubocop:disable Style/FileNull
